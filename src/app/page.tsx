d40b95cfef89ef2b34cdc84c712a68502b08fa24
guiireal <guii_real@hotmail.com>
2024-12-30 01:20:28 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
-export default function Home() {
-  return <main className="flex">Oi</main>;
+import { Container } from "@/components/Container";
+import { Game } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRightSquare } from "react-icons/bs";
+
+async function getDailyGame() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      {
+        next: {
+          revalidate: 320,
+        },
+      }
+    );
+
+    return response.json();
+  } catch (error) {
+    throw new Error("Falied to fetch data");
+  }
+}
+
+export default async function Home() {
+  const dailyGame: Game = await getDailyGame();
+
+  return (
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo pra você
+        </h1>
+        <Link href={`/game/${dailyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">
+                  {dailyGame.title}
+                </p>
+                <BsArrowRightSquare size="24" color="#FFF" />
+              </div>
+              <Image
+                src={dailyGame.image_url}
+                alt={dailyGame.title}
+                priority
+                quality={100}
+                fill
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
+    </main>
+  );
 }
