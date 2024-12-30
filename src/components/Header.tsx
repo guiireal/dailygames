957358bd481c473b60250a147de133ca42067e80
guiireal <guii_real@hotmail.com>
2024-12-29 23:49:33 -0300

    wip

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import { LiaGamepadSolid } from "react-icons/lia";
+import logo from "../../public/logo.svg";
+
+export function Header() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              className="w-full"
+              src={logo}
+              alt="Logo"
+              quality={100}
+              priority
+            />
+          </Link>
+          <Link href="/">Games</Link>
+          <Link href="/profile">Perfil</Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <LiaGamepadSolid size="34" color="#475569" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
