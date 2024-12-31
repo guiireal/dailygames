7b0aa337340bf306c8c366f48d91ffe332067a9d
guiireal <guii_real@hotmail.com>
2024-12-30 22:19:19 -0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dailygames",
+  title: "Dalygames",
   description: "Jogos diários",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Container } from "@/components/Container";
+import { GameCard } from "@/components/GameCard";
 import { Input } from "@/components/Input";
 import { Game } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { BsArrowRightSquare } from "react-icons/bs";
 
-async function getDailyGame() {
+async function getDalyGame() {
   try {
     const response = await fetch(
       `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
@@ -22,8 +23,26 @@ async function getDailyGame() {
   }
 }
 
+async function getGamesData() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=games`,
+      {
+        next: {
+          revalidate: 320,
+        },
+      }
+    );
+
+    return response.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
 export default async function Home() {
-  const dailyGame: Game = await getDailyGame();
+  const dalyGame: Game = await getDalyGame();
+  const data: Game[] = await getGamesData();
 
   return (
     <main className="w-full">
@@ -31,28 +50,32 @@ export default async function Home() {
         <h1 className="text-center font-bold text-xl mt-8 mb-5">
           Separamos um jogo exclusivo pra você
         </h1>
-        <Link href={`/game/${dailyGame.id}`}>
+        <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
             <div className="w-full max-h-96 h-96 relative rounded-lg">
               <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
-                <p className="font-bold text-xl text-white">
-                  {dailyGame.title}
-                </p>
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
                 <BsArrowRightSquare size="24" color="#FFF" />
               </div>
               <Image
-                src={dailyGame.image_url}
-                alt={dailyGame.title}
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
                 priority
                 quality={100}
                 fill
                 className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
               />
             </div>
           </section>
         </Link>
         <Input />
+        <h2 className="text-lg font-bold mt-8 mb-5">Jogos para conhecer</h2>
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map((game) => (
+            <GameCard key={game.id} data={game} />
+          ))}
+        </section>
       </Container>
     </main>
   );
